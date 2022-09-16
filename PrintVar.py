9a1c9611adc5f76dbd9a3e8c3ff881ef55bab1ef
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-09-16 18:58:59 -0300

    Create PrintVar.py

---
+++ PrintVar.py
@@ -0,0 +1,6 @@
+n1 = int(input())
+n2 = int(input())
+if(n1*n2<=1000): 
+    print("The multiplication of %i and %i is %i"%(n1, n2, n1*n2))
+else:
+    print("The sum of %i and %i is %i"%(n1, n2, n1+n2))
